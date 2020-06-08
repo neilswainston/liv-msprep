0d25e0c3e612d52f8f6a81ab334c32610b221de8
Neil Swainston <neil.swainston@liverpool.ac.uk>
2020-06-08 13:58:38 +0100

    Update based on initial tests.

--- liv_msprep/msprep.py
+++ liv_msprep/msprep.py
@@ -18,10 +18,10 @@ metadata = {'apiLevel': '2.3',
 
 
 _REAGENT_PLATE_TYPE = 'agilent_1_reservoir_290ml'
-_SAMPLE_PLATE_TYPE = 'opentrons_24_aluminumblock_nest_1.5ml_snapcap'
-_DEST_PLATE_TYPE = 'thermo_96_wellplate_450ul'
+_SAMPLE_PLATE_TYPE = 'opentrons_24_aluminumblock_nest_2ml_snapcap'
+_DEST_PLATE_TYPE = '4titude_96_wellplate_350ul'
 
-_SRC_PLATE_LAST = 'D6'
+_SRC_PLATE_LAST = 'A2'
 _NUM_REPS = 3
 
 
@@ -30,52 +30,33 @@ def run(protocol):
 
     # Setup:
     p300_single, p300_multi, reag_plt, src_plt, dest_plt = _setup(protocol)
+
     num_src = src_plt.wells().index(src_plt[_SRC_PLATE_LAST]) + 1
     num_repl_wells = len(src_plt.wells()) * _NUM_REPS
     pool_col_idx = num_repl_wells // len(dest_plt.rows())
 
     # Plate:
-    for src_idx, src_well in enumerate(src_plt.wells()[:num_src]):
-        repl_idxs = range(src_idx,
-                          num_repl_wells,
-                          len(src_plt.wells()))
-
-        p300_single.distribute(
-            75,
-            src_well,
-            [dest_plt.wells()[idx] for idx in repl_idxs],
-            # air_gap=20,
-            disposal_volume=0,
-            trash=False)
+    protocol.comment('\nPlate')
+    _plate(p300_single, src_plt, dest_plt, num_src, num_repl_wells)
 
     protocol.pause('Put %s in vacuum drier.\n' % dest_plt)
 
     # Resuspend:
-    for col_idx in range(_NUM_REPS):
-        repl_idxs = range(col_idx, pool_col_idx, _NUM_REPS)
-        repl_wells = [dest_plt.columns()[idx] for idx in repl_idxs]
+    protocol.comment('\nResuspend')
+    _resuspend(p300_multi, reag_plt, dest_plt, num_src, pool_col_idx)
+    p300_multi.reset_tipracks()
 
-        p300_multi.transfer(
-            40,
-            reag_plt['A1'],
-            repl_wells,
-            mix_after=(3, 40)
-        )
+    # Mix:
+    protocol.comment('\nMix')
+    _mix(p300_multi, dest_plt, num_src, pool_col_idx)
+    p300_multi.reset_tipracks()
 
     protocol.pause(
         'Centrifuge %s (remove bubbles and any particulates).\n' % dest_plt)
 
-    p300_single.reset_tipracks()
-
     # Pool:
-    for col_idx in range(_NUM_REPS):
-        repl_idxs = range(col_idx, pool_col_idx, _NUM_REPS)
-        repl_wells = [dest_plt.columns()[idx] for idx in repl_idxs]
-
-        p300_multi.consolidate(
-            40,
-            repl_wells,
-            dest_plt.columns()[pool_col_idx + col_idx])
+    protocol.comment('\nPool')
+    _pool(p300_multi, dest_plt, num_src, pool_col_idx)
 
 
 def _setup(protocol):
@@ -90,10 +71,10 @@ def _setup(protocol):
 
     # Add pipette:
     p300_single = protocol.load_instrument(
-        'p300_single', 'left', tip_racks=tip_racks_200)
+        'p300_single', 'right', tip_racks=tip_racks_200)
 
     p300_multi = protocol.load_instrument(
-        'p300_multi', 'right', tip_racks=tip_racks_200)
+        'p300_multi', 'left', tip_racks=tip_racks_200)
 
     # Add plates:
     reag_plt = protocol.load_labware(_REAGENT_PLATE_TYPE, 9, 'reagent')
@@ -101,6 +82,66 @@ def _setup(protocol):
     dest_plt = protocol.load_labware(_DEST_PLATE_TYPE, 8, 'destination')
 
     return p300_single, p300_multi, reag_plt, src_plt, dest_plt
+
+
+def _plate(pipette, src_plt, dest_plt, num_src, num_repl_wells):
+    '''Plate.'''
+    for src_idx, src_well in enumerate(src_plt.wells()[:num_src]):
+        repl_idxs = range(src_idx,
+                          num_repl_wells,
+                          len(src_plt.wells()))
+
+        pipette.distribute(
+            75,
+            src_well,
+            [dest_plt.wells()[idx] for idx in repl_idxs],
+            disposal_volume=0,
+            trash=False)
+
+
+def _resuspend(pipette, reag_plt, dest_plt, num_src, pool_col_idx):
+    '''Resuspend.'''
+    pipette.pick_up_tip()
+
+    for col_idx in range(min(num_src // len(dest_plt.rows()) + 1, _NUM_REPS)):
+        repl_idxs = range(col_idx, pool_col_idx, _NUM_REPS)
+        repl_wells = [dest_plt.columns()[idx][0] for idx in repl_idxs]
+
+        pipette.distribute(
+            40,
+            reag_plt['A1'],
+            [wells.top() for wells in repl_wells],
+            disposal_volume=0,
+            new_tip='never'
+        )
+
+    pipette.drop_tip()
+
+
+def _mix(pipette, dest_plt, num_src, pool_col_idx):
+    '''Mix.'''
+    for col_idx in range(min(num_src // len(dest_plt.rows()) + 1, _NUM_REPS)):
+        repl_idxs = range(col_idx, pool_col_idx, _NUM_REPS)
+        repl_wells = [dest_plt.columns()[idx][0] for idx in repl_idxs]
+
+        pipette.pick_up_tip()
+
+        for well in repl_wells:
+            pipette.mix(3, 40, well)
+
+        pipette.return_tip()
+
+
+def _pool(pipette, dest_plt, num_src, pool_col_idx):
+    '''Pool.'''
+    for col_idx in range(min(num_src // len(dest_plt.rows()) + 1, _NUM_REPS)):
+        repl_idxs = range(col_idx, pool_col_idx, _NUM_REPS)
+        repl_wells = [dest_plt.columns()[idx] for idx in repl_idxs]
+
+        pipette.consolidate(
+            40,
+            repl_wells,
+            dest_plt.columns()[pool_col_idx + col_idx])
 
 
 def main():
